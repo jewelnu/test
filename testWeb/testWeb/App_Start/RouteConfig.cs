4a3eddeda83fa44f5238654c08ec1280f1f796e5
jewel <jewelice2006@gmail.com>
2014-06-27 21:17:52 +0600

    Shared page correction

--- testWeb/testWeb/App_Start/RouteConfig.cs
+++ testWeb/testWeb/App_Start/RouteConfig.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using testWeb.Models;
 
 namespace testWeb
 {
@@ -11,6 +13,7 @@ namespace testWeb
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            Database.SetInitializer(new SampleSeedData());
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
